d439ee2912fecfb0baf3f9526210d6a07de8ecec
Tiger Guo <teguo@microsoft.com>
2025-10-25 16:27:42 +0800

    init

---
+++ ios/PoemMedia/PoemMedia/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  PoemMedia
+//
+//  Created by tigerguo on 2025/10/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ ios/PoemMedia/PoemMediaTests/PoemMediaTests.swift
@@ -0,0 +1,16 @@
+//
+//  PoemMediaTests.swift
+//  PoemMediaTests
+//
+//  Created by tigerguo on 2025/10/25.
+//
+
+import Testing
+
+struct PoemMediaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
